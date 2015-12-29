90d3f5d7139d1fcc161711df971fbbe1457add20
scratz <scratz@sbcglobal.net>
2015-12-29 15:44:18 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once 'scratzadmin/cms.php' ?>
+<cms:smart_embed 'page-head' />
+<cms:smart_embed 'head-nav' />
+<cms:smart_embed 'index' />
+<cms:smart_embed 'footer' />
+</body><!-- END BODY -->
+</html>
+<?php COUCH::invoke(); ?>
